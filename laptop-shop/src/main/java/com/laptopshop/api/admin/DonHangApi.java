e59b5293eadcf479a3ef75a4be335b56a7d05e49
nguyentuanbkhn <nguyentuandt09@gmail.com>
2023-10-10 14:56:42 +0700

    up

--- laptop-shop/src/main/java/com/laptopshop/api/admin/DonHangApi.java
+++ laptop-shop/src/main/java/com/laptopshop/api/admin/DonHangApi.java
@@ -125,7 +125,6 @@ public class DonHangApi {
 	public String getMaVaiTroAPI(@RequestParam("idnguoidung") Long idnguoidung) {
 		String mavaitro = donHangService.getMaVaiTroService(idnguoidung);
 		System.out.println(mavaitro);
-		//TuanNV test laptopshop1
 		return mavaitro;
 	}
 }
